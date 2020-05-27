a38b1267abd61e2da56daff346836190bcfa176f
rebeldevelop <adrianyellowworm@gmail.com>
2020-05-27 20:52:10 +0200

    creacion de scroll navigation

--- vinosydelicatessendecartagenaweb/src/router/index.js
+++ vinosydelicatessendecartagenaweb/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import ScrollPrueba from '@/components/ScrollPrueba'
+import Main from '@/components/Main'
 
 Vue.use(Router)
 
@@ -8,8 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'ScrollPrueba',
-      component: ScrollPrueba
+      name: 'Main',
+      component: Main
     }
   ]
 })
