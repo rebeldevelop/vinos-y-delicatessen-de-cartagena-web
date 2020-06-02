bcbc8dbc49486458f2967b23d8971e7a2c9ed1d2
rebeldevelop <adrianyellowworm@gmail.com>
2020-06-02 14:22:45 +0200

    elementos arreglados

--- vinosydelicatessendecartagenaweb/functions/index.js
+++ vinosydelicatessendecartagenaweb/functions/index.js
@@ -1,8 +1,8 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-const nodemailer = require('nodemailer');
-const cors = require('cors')({origin: true});
-admin.initializeApp();
+const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+const nodemailer = require('nodemailer')
+const cors = require('cors')({origin: true})
+admin.initializeApp()
 
 /**
 * Here we're using Gmail to send
@@ -13,12 +13,12 @@ let transporter = nodemailer.createTransport({
     user: 'rebeldevelop@gmail.com',
     pass: 'embudopeloti1'
   }
-});
+})
 
 exports.sendMail = functions.https.onRequest((req, res) => {
   cors(req, res, () => {
     // getting dest email by query string
-    const dest = req.query.dest;
+    const dest = req.query.dest
 
     const mailOptions = {
       from: 'RebelDevelop<rebeldevelop@gmail.com>', // Something like: Jane Doe <janedoe@gmail.com>
@@ -26,14 +26,14 @@ exports.sendMail = functions.https.onRequest((req, res) => {
       subject: 'Rebel Develop te envía tol imeil', // email subject
       html: `<p style="font-size: 16px;"></p>
             ` // email content in HTML
-    };
+    }
 
-      // returning result
+    // returning result
     return transporter.sendMail(mailOptions, (erro, info) => {
       if (erro) {
-        return res.send(erro.toString());
+        return res.send(erro.toString())
       }
-      return res.send('Sended');
-    });
-  });
-});
+      return res.send('Sended')
+    })
+  })
+})
